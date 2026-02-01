478df22b53e9ef107c7d75d1bcc9b23461fe3744
Catalin-Stan-0000 <c00320319@setu.ie>
2026-02-01 10:04:13 +0000

    Added non-functional cursor class

---
+++ yourHammerMyChisel/Cursor.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "GameState.h"
+
+class Cursor
+{
+public:
+	void Start();
+	void Update();
+	void Render(sf::RenderWindow& t_window);
+	void displayCursor();
+private:
+
+};
